7c293016f0bb808151b1d9a0c2b8bc7298dc2bc4
“Liam <okeefe.liam@yahoo.com>
2022-09-14 16:35:11 -0500

    commit tree fa371f9644fe3bcc94c79653282aed295536b2f6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+   return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase()) ///strict equal - return value array
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.slice(0, letters.length) === letters)
+        // return driver.slice(0, letters.length) === letters
+    // give a starting and ending point or it'll start at point and go until end
+    // letter = "As" = first 2 of Ashley
+    // drivers[0] = "A"
+} 
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
+}
